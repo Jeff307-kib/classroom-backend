06db976b75bde1beecf7a71d4f5929525050215e
Jeff307-kib <wunnatunsai940@gmail.com>
2026-06-27 20:54:51 +0630

    Implement get all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
 import express from "express";
+import subjectRouter from "./routes/subjects.js";
+import cors from "cors";
 
-const PORT = 8000;
 const app = express();
+const PORT = 8000;
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 
 app.use(express.json());
 
 app.use((req, res, next) => {
     const timestamp = new Date().toISOString();
     console.log(`${req.method} | ${req.url} | ${timestamp}`);
+    next();
 });
 
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send("Hello World!");
-});
-
-app.use('/api/v1', router);
+app.use('/api/subjects', subjectRouter);
 
 app.listen(PORT, () => {
     console.log(`SERVER running on PORT${PORT}`);
